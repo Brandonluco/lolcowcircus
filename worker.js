9c2288a046f3a9979edc8eafe328b010f5a06acc
Brandonluco <293171896+Brandonluco@users.noreply.github.com>
2026-08-09 13:09:53 -0400

    Enabling Global Comments Through worker.js

--- worker.js
+++ worker.js
@@ -1,5 +1,61 @@
 export default {
   async fetch(request, env) {
+
+    const url = new URL(request.url);
+
+    // =====================
+    // COMMENTS API
+    // =====================
+
+    if (url.pathname === "/api/comments") {
+
+      // GET comments
+      if (request.method === "GET") {
+
+        const { results } = await env.DB
+          .prepare(
+            "SELECT * FROM comments ORDER BY id DESC"
+          )
+          .all();
+
+        return Response.json(results);
+      }
+
+
+      // POST comment
+      if (request.method === "POST") {
+
+        const data = await request.json();
+
+        await env.DB
+          .prepare(
+            `
+            INSERT INTO comments
+            (username, message, color, created_at)
+            VALUES (?, ?, ?, ?)
+            `
+          )
+          .bind(
+            data.username,
+            data.message,
+            data.color,
+            data.created_at
+          )
+          .run();
+
+
+        return Response.json({
+          success: true
+        });
+      }
+    }
+
+
+    // =====================
+    // WEBSITE FILES
+    // =====================
+
     return env.ASSETS.fetch(request);
+
   }
 };
